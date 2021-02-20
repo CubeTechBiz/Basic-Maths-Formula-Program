3253c0979d7f9ab4a55ffdf03fbb828d416539bd
CubeTechBiz <77924606+CubeTechBiz@users.noreply.github.com>
2021-02-20 16:41:50 +0530

    Simple interest program

---
+++ Simple_interest.py
@@ -0,0 +1,7 @@
+p=float(input("Please enter principle amount: "))
+t=float(input("Please enter the time in years: "))
+r=float(input("Please enter the rate of interest: "))
+
+si=(p*t*r)/100;        #si is stand for simple interest
+
+print("\n simple interest is: ",si);
